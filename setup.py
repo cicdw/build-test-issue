4c480673dd1e92659eca49a92de9b26f78aad93e
Chris White <chris@prefect.io>
2018-07-19 21:44:55 -0700

    Commit of top level files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="build_test",
+    description="",
+    url="https://github.com/cicdw/build-test-issue",
+    author="Chris White",
+    author_email="white.cdw@gmail.com",
+    scripts=[],
+    packages=find_packages(),
+    include_package_data=True,
+)
